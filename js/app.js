4ef13fab432eb90533a9e08d1e7b09f1dae88216
Radek Stepan <radek.stepan@gmail.com>
2011-11-21 20:58:35 +0000

    extra comments

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 $(function(){
 
 	// Todo Model
+	// Models are the heart of any JavaScript application, containing the interactive data as well as a large part
+	//  of the logic surrounding it: conversions, validations, computed properties, and access control.
 	// ----------
 
 	// Our basic **Todo** model has `text`, `order`, and `done` attributes.
@@ -9,52 +11,57 @@ $(function(){
 
 		// Default attributes for a todo item.
 		defaults: function() {
+			// `text` attr is implied
 			return {
-				done:  false,
-				order: Todos.nextOrder()
+				"done":  false,
+				"order": Todos.nextOrder()
 			};
 		},
 
 		// Toggle the `done` state of this todo item.
 		toggle: function() {
-			this.save({done: !this.get("done")});
+			this.save({"done": !this.get("done")});
 		}
 
 	});
 
 	// Todo Collection
+	// Collections are ordered sets of models
 	// ---------------
 
-	// The collection of todos is backed by *localStorage* instead of a remote
-	// server.
+	// The collection of todos is backed by *localStorage* instead of a remote server.
 	window.TodoList = Backbone.Collection.extend({
 
 		// Reference to this collection's model.
-		model: Todo,
+		// Override this property to specify the model class that the collection contains.
+		"model": Todo,
 
 		// Save all of the todo items under the `"todos"` namespace.
-		localStorage: new Store("todos"),
+		"localStorage": new Store("todos"),
 
 		// Filter down the list of all todo items that are finished.
 		done: function() {
-			return this.filter(function(todo){ return todo.get('done'); });
+			return this.filter(function(todo) {
+				return todo.get("done"); // all items with `done` attr set to true
+			});
 		},
 
 		// Filter down the list to only todo items that are still not finished.
 		remaining: function() {
-			return this.without.apply(this, this.done());
+			return this.without.apply(this, this.done()); // reverse of `done()` filter
 		},
 
 		// We keep the Todos in sequential order, despite being saved by unordered
 		// GUID in the database. This generates the next order number for new items.
 		nextOrder: function() {
-			if (!this.length) return 1;
-			return this.last().get('order') + 1;
+			if (!this.length) return 1; // first item
+			return this.last().get("order") + 1; // last +1
 		},
 
 		// Todos are sorted by their original insertion order.
+		// If you define a comparator, it will be used to maintain the collection in sorted order.
 		comparator: function(todo) {
-			return todo.get('order');
+			return todo.get("order");
 		}
 
 	});
@@ -66,16 +73,17 @@ $(function(){
 	// --------------
 
 	// The DOM element for a todo item...
+	// Not a strict convention
 	window.TodoView = Backbone.View.extend({
 
 		//... is a list tag.
-		tagName:  "li",
+		"tagName":  "li",
 
 		// Cache the template function for a single item.
-		template: _.template($('#item-template').html()),
+		"template": _.template($('#item-template').html()),
 
 		// The DOM events specific to an item.
-		events: {
+		"events": {
 			"click .check"              : "toggleDone",
 			"dblclick div.todo-text"    : "edit",
 			"click span.todo-destroy"   : "clear",
@@ -83,14 +91,16 @@ $(function(){
 		},
 
 		// The TodoView listens for changes to its model, re-rendering.
+		// If the view defines an initialize function, it will be called when the view is first created.
 		initialize: function() {
-			this.model.bind('change', this.render, this);
-			this.model.bind('destroy', this.remove, this);
+			this.model.bind("change", this.render, this);
+			this.model.bind("destroy", this.remove, this);
 		},
 
 		// Re-render the contents of the todo item.
+		// Override this function with your code that renders the view template from model data, and updates this.el with the new HTML.
 		render: function() {
-			$(this.el).html(this.template(this.model.toJSON()));
+			$(this.el).html(this.template(this.model.toJSON())); // serialize to JSON, fill tml, set as innerHTML
 			this.setText();
 			return this;
 		},
@@ -98,7 +108,9 @@ $(function(){
 		// To avoid XSS (not that it would be harmful in this particular app),
 		// we use `jQuery.text` to set the contents of the todo item.
 		setText: function() {
+			// get attr from the model
 			var text = this.model.get('text');
+			// set the text on the element
 			this.$('.todo-text').text(text);
 			this.input = this.$('.todo-input');
 			this.input.bind('blur', _.bind(this.close, this)).val(text);
@@ -133,6 +145,7 @@ $(function(){
 
 		// Remove the item, destroy the model.
 		clear: function() {
+			// HTTP DELETE
 			this.model.destroy();
 		}
 
@@ -146,13 +159,14 @@ $(function(){
 
 		// Instead of generating a new element, bind to the existing skeleton of
 		// the App already present in the HTML.
-		el: $("#todoapp"),
+		"el": $("#todoapp"),
 
 		// Our template for the line of statistics at the bottom of the app.
-		statsTemplate: _.template($('#stats-template').html()),
+		// Compiles JavaScript templates into functions that can be evaluated for rendering.
+		"statsTemplate": _.template($('#stats-template').html()),
 
 		// Delegated events for creating new items, and clearing completed ones.
-		events: {
+		"events": {
 			"keypress #new-todo":  "createOnEnter",
 			"keyup #new-todo":     "showTooltip",
 			"click .todo-clear a": "clearCompleted"
@@ -164,9 +178,9 @@ $(function(){
 		initialize: function() {
 			this.input = this.$("#new-todo");
 
-			Todos.bind('add',   this.addOne, this);
-			Todos.bind('reset', this.addAll, this);
-			Todos.bind('all',   this.render, this);
+			Todos.bind("add",   this.addOne, this);
+			Todos.bind("reset", this.addAll, this);
+			Todos.bind("all",   this.render, this);
 
 			Todos.fetch();
 		},
@@ -174,10 +188,10 @@ $(function(){
 		// Re-rendering the App just means refreshing the statistics -- the rest
 		// of the app doesn't change.
 		render: function() {
-			this.$('#todo-stats').html(this.statsTemplate({
-			total:      Todos.length,
-			done:       Todos.done().length,
-			remaining:  Todos.remaining().length
+			this.$("#todo-stats").html(this.statsTemplate({
+				"total":      Todos.length,
+				"done":       Todos.done().length,
+				"remaining":  Todos.remaining().length
 			}));
 		},
 
@@ -198,6 +212,7 @@ $(function(){
 		createOnEnter: function(e) {
 			var text = this.input.val();
 			if (!text || e.keyCode != 13) return;
+			
 			Todos.create({text: text});
 			this.input.val('');
 		},
